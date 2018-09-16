e584db4a8ef492c334a895075d310bc0989287a6
Oxy976 <ghost_mail2005-musor@yahoo.com>
2018-09-16 13:42:20 +0300

    +RUS font

--- Morse trainer.ino
+++ Morse trainer.ino
@@ -197,12 +197,12 @@ void loop() {
     M5.Lcd.setTextColor(0x0000);
     M5.Lcd.setTextSize(5);
     M5.Lcd.setCursor(60, 55);
-    M5.Lcd.print(MorseTelephony[i][0]);
+    M5.Lcd.print(utf8rus(MorseTelephony[i])[0]);
     M5.Lcd.setTextSize(2);
     M5.Lcd.setCursor(120, 50);
-    M5.Lcd.print(MorseTelephony[i]);
+    M5.Lcd.print(utf8rus(MorseTelephony[i]));
     M5.Lcd.setCursor(120, 80);
-    M5.Lcd.print(MorsePhonic[i]);
+    M5.Lcd.print(utf8rus(MorsePhonic[i]));
     Play(i);
     while (true)
     {
@@ -265,4 +265,40 @@ void loop() {
         break;
     }
   }
+}
+
+
+/* Recode russian fonts from UTF-8 to Windows-1251 */
+
+String utf8rus(String source)
+{
+  int i,k;
+  String target;
+  unsigned char n;
+  char m[2] = { '0', '\0' };
+
+  k = source.length(); i = 0;
+
+  while (i < k) {
+    n = source[i]; i++;
+
+    if (n >= 0xC0) {
+      switch (n) {
+        case 0xD0: {
+          n = source[i]; i++;
+          if (n == 0x81) { n = 0xA8; break; }
+          if (n >= 0x90 && n <= 0xBF) n = n + 0x30;
+          break;
+        }
+        case 0xD1: {
+          n = source[i]; i++;
+          if (n == 0x91) { n = 0xB8; break; }
+          if (n >= 0x80 && n <= 0x8F) n = n + 0x70;
+          break;
+        }
+      }
+    }
+    m[0] = n; target = target + String(m);
+  }
+return target;
 }
